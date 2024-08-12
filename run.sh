0a28aad461786f84a504cedff8097ff62847fa55
sujantkumarkv <sujantdev@gmail.com>
2024-08-12 22:08:19 +0530

    add tags etc

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # Navigate to your application directory (if necessary)
 cd prabhav/
 
+# virtual env
+python3 -m venv venv && source venv/bin/activate
+
 # install dependencies
 pip install -r requirements.txt
 
