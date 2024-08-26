303ac719337ac08bc392fa7132eeac841bda06ea
sujantkumarkv <sujantdev@gmail.com>
2024-08-26 14:16:44 +0530

    update recipient_waid

--- prabhav/app/config.py
+++ prabhav/app/config.py
@@ -10,7 +10,7 @@ def load_configurations(app):
     app.config["YOUR_PHONE_NUMBER"] = os.getenv("YOUR_PHONE_NUMBER")
     app.config["APP_ID"] = os.getenv("APP_ID")
     app.config["APP_SECRET"] = os.getenv("APP_SECRET")
-    app.config["RECIPIENT_WAID"] = os.getenv("RECIPIENT_WAID")
+    # app.config["RECIPIENT_WAID"] = os.getenv("RECIPIENT_WAID")
     app.config["VERSION"] = os.getenv("VERSION")
     app.config["PHONE_NUMBER_ID"] = os.getenv("PHONE_NUMBER_ID")
     app.config["VERIFY_TOKEN"] = os.getenv("VERIFY_TOKEN")
